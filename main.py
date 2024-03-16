b77046a693054160023b3766d273e145fc21faa8
ztttas1 <125969373+ztttas1@users.noreply.github.com>
2024-03-16 22:51:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import time
 async def reconnect():
   while True:
     
-    await asyncio.sleep(600)
+    await asyncio.sleep(900)
     requests.get("https://api.render.com/deploy/srv-cnqlbhn109ks73fcbbp0?key=5QJY5VfBmLs")
 
 
