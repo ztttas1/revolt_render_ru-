d66748fc7729bba48ed2a9d4adae9801be2a023c
ztttas1 <125969373+ztttas1@users.noreply.github.com>
2024-03-20 21:43:49 +0900

    Update always_on.py

--- always_on.py
+++ always_on.py
@@ -1,18 +1,11 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
-
+app = Flask(__name__)
 
 @app.route('/')
-def main():
-  return 'temp'
+def serve_log_file():
+    return send_from_directory('.', 'roulette_log.txt')
 
-
-def run():
-  app.run(host="0.0.0.0", port=8080)
-
-
-def activate():
-  server = Thread(target=run)
-  server.start()
+if __name__ == "__main__":
+    app.run(port=8000)
