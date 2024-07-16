2f4dbd9219487523afdfacaf06d8524bf3d39f87
ztttas1 <125969373+ztttas1@users.noreply.github.com>
2024-07-16 14:03:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ class Client(revolt.Client):
       
     if message.content == '.g':
       numbers = [random.randint(0, 9) for _ in range(10)]
-
       # 生成された乱数が全て同じかどうかチェック
       if numbers[0] == numbers[1] == numbers[2] == numbers[3] == numbers[4] == numbers[5] == numbers[6] == numbers[7] == numbers[8] == numbers[9]:
           # Revoltのチャンネルに"good"と送信
@@ -40,7 +39,7 @@ class Client(revolt.Client):
           # 乱数が異なる場合は、その乱数を送信
           await message.channel.send(f"> Generated numbers: {numbers}")
           print(f"{numbers} - NO")
-      if message.content == '.help':
+    if message.content == '.help':
         await message.channel.send(f"> ##  Rouletto\n > ### You can play roulette\n > Normal mode:$ .r $ \n > Hard mode:$ .g $ \n Help:$ .help $\n ##### Management:<@01HJQHPJKEERVFMY165QGVF6DC> | <@01HS541Y8H5TQMWVEQD2VK213M>")
         
       
