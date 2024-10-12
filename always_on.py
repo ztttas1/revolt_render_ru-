fc23a5cfea6fd719ab669fd578d76cf36e7da164
ztttas1 <125969373+ztttas1@users.noreply.github.com>
2024-10-12 12:46:48 +0900

    Update always_on.py

--- always_on.py
+++ always_on.py
@@ -6,7 +6,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-  
+  asyncio.run(main())
   return '<h1>Page is under construction.</h1><br><a href="https://app.revolt.chat/bot/01HS541Y8H5TQMWVEQD2VK213M">Invite</a><br><a href="https://revoltbots.org/bots/01HS541Y8H5TQMWVEQD2VK213M/vote">Vote</a><br>Used in {len(guilds)} servers!'
 
 
